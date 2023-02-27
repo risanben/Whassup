788521c8c3e14a3e44670fe360077f8e866806bc
risanBenichou <ris.benichou@gmail.com>
2023-02-27 18:33:46 +0200

    side bar - beginning

--- frontend/src/cmps/app-header.jsx
+++ frontend/src/cmps/app-header.jsx
@@ -1,20 +1,24 @@
 import { Link, NavLink } from 'react-router-dom'
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
 import routes from '../routes'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 import { login, logout, signup } from '../store/user.actions.js'
 import { LoginSignup } from './login-signup.jsx'
 import { useEffect } from 'react'
+import { BiDotsVerticalRounded } from 'react-icons/bi'
+import { BsChatLeftDotsFill } from 'react-icons/bs'
+import { BsFilter } from 'react-icons/bs'
+
+
 
 export function AppHeader() {
     const user = useSelector(storeState => storeState.userModule.user)
 
     useEffect(() => {
         loadChats()
-    }, [])
+    }, [user])
     async function loadChats() {
         console.log('user', user)
-
         // try {
         //     const user = await login(credentials)
         //     showSuccessMsg(`Welcome: ${user.fullname}`)
@@ -26,7 +30,7 @@ export function AppHeader() {
         try {
             const user = await login(credentials)
             showSuccessMsg(`Welcome: ${user.fullname}`)
-        } catch(err) {
+        } catch (err) {
             showErrorMsg('Cannot login')
         }
     }
@@ -34,7 +38,7 @@ export function AppHeader() {
         try {
             const user = await signup(credentials)
             showSuccessMsg(`Welcome new user: ${user.fullname}`)
-        } catch(err) {
+        } catch (err) {
             showErrorMsg('Cannot signup')
         }
     }
@@ -42,17 +46,38 @@ export function AppHeader() {
         try {
             await logout()
             showSuccessMsg(`Bye now`)
-        } catch(err) {
+        } catch (err) {
             showErrorMsg('Cannot logout')
         }
     }
 
     return (
         <header className="app-header">
+            <section className="top-section flex">
+                {user &&
+                    <>
+                        {user.imgUrl && <img src={user.imgUrl} />}
+                        <section className='menu-btns flex '>
+                            <BsChatLeftDotsFill className='chat-btn' />
+                            <BiDotsVerticalRounded className='menu-btn' />
+                        </section>
+                    </>}
+            </section>
+
+            <section className="chat-search flex align-center space-between">
+                <section className="input-section flex">
+                    <input type="text" placeholder='Search in chats' />
+                </section>
+                <BsFilter className='chat-filter-btn' />
+            </section>
+
+            <section className="archive">
+
+            </section>
             <nav>
                 {/* {routes.map(route => <NavLink key={route.path} to={route.path}>{route.label}</NavLink>)} */}
 
-                {user &&
+                {/* {user &&
                     <span className="user-info">
                         <Link to={`user/${user._id}`}>
                             {user.imgUrl && <img src={user.imgUrl} />}
@@ -61,7 +86,7 @@ export function AppHeader() {
                         <span className="score">{user.score?.toLocaleString()}</span>
                         <button onClick={onLogout}>Logout</button>
                     </span>
-                }
+                } */}
                 {!user &&
                     <section className="user-info">
                         <LoginSignup onLogin={onLogin} onSignup={onSignup} />
